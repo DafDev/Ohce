e37932bcb8499b8b7f2459377675ee484ebd1668
Daf <afahd.ali@gmail.com>
2024-11-20 12:09:45 +0100

    feat: add reversor use case

---
+++ src/Daf.Oche.Console/Reversor.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Daf.Oche.Console;
+public class Reversor
+{
+    public (string actual, bool isActualPallindrome) Reverse(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return (string.Empty, false);
+        var reversed = string.Join(string.Empty, input.Reverse().ToList()) ?? string.Empty;
+        return (reversed, input == reversed);
+    }
+}
---
+++ tests/Daf.Oche.Console.Tests/ReversorTests.cs
@@ -0,0 +1,20 @@
+﻿using Daf.Oche.Console.Tests.TestData;
+using FluentAssertions;
+
+namespace Daf.Oche.Console.Tests;
+public class ReversorTests
+{
+    private readonly Reversor _sut = new();
+
+    [Theory]
+    [ClassData(typeof(ReversorTestsData))]
+    public void GivenWordWhenReverseShouldReturnReversedWordAndIfPallindrome(string input,string expected,bool isExpectedPallindrome)
+    {
+        // When
+        (string actual, bool isActualPallindrome) = _sut.Reverse(input);
+
+        // Should
+        actual.Should().Be(expected);
+        isActualPallindrome.Should().Be(isExpectedPallindrome);
+    }
+}
